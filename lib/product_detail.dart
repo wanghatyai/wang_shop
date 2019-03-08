86c8841e595a74ca98731caf128af93a2bfb0dd2
wangAppPC <wanghatyai@gmail.com>
2019-03-08 18:09:37 +0700

    add Select unit function in product Detail

--- lib/product_detail.dart
+++ lib/product_detail.dart
@@ -8,6 +8,7 @@ class productDetailPage extends StatefulWidget {
   var product;
   productDetailPage({Key key, this.product}) : super(key: key);
 
+
   @override
   _productDetailPageState createState() => _productDetailPageState();
 }
@@ -15,47 +16,133 @@ class productDetailPage extends StatefulWidget {
 class _productDetailPageState extends State<productDetailPage> {
   @override
 
+  //List<DropdownMenuItem<String>> units = [];
+  List units = [];
+  String _currentUnit;
+
+  /*loadUnits(){
+    units = [];
+    //_currentUnit = widget.product['unit1'].toString();
+
+    if(widget.product['unit1'].toString() != "null"){
+
+      units.add(DropdownMenuItem(
+        child: Text(widget.product['unit1'].toString()),
+        value: widget.product['unit1'].toString(),
+      ));
+    }
+    if(widget.product['unit2'].toString() != "null"){
+      units.add(DropdownMenuItem(
+        child: Text(widget.product['unit2'].toString()),
+        value: widget.product['unit2'].toString(),
+      ));
+    }
+    if(widget.product['unit3'].toString() != "null"){
+      units.add(DropdownMenuItem(
+        child: Text(widget.product['unit3'].toString()),
+        value: widget.product['unit3'].toString(),
+      ));
+    }
+  }*/
+
+
+
   Widget build(BuildContext context) {
 
-    //print(widget.product);
+    units = [];
+
+    if(widget.product['unit1'].toString() != "null"){
+      units.add(widget.product['unit1'].toString());
+    }
+    if(widget.product['unit2'].toString() != "null"){
+      units.add(widget.product['unit2'].toString());
+    }
+    if(widget.product['unit3'].toString() != "null"){
+      units.add(widget.product['unit3'].toString());
+    }
+
+    //loadUnits();
+    //print(widget.product['unit3'].toString());
+    //print(_currentUnit);
+    //print(_units);
 
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.product['nproductMain'].toString()),
       ),
-      body: Column(
-        children: <Widget>[
-          Image.network('http://www.wangpharma.com/cms/product/${widget.product['pic']}',fit: BoxFit.contain, width:double.infinity, height: 250,),
-          Text("รหัสสินค้า : ${widget.product['pcode']}"),
-          Padding(
-            padding: EdgeInsets.all(4),
-            child: Text(
-                widget.product['nproductMain'].toString(),
-                style: new TextStyle(
-                  fontSize: 16
+      body: Container(
+        padding: const EdgeInsets.all(5),
+        child: Row(
+          children: <Widget>[
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Image.network('http://www.wangpharma.com/cms/product/${widget.product['pic']}',fit: BoxFit.contain, width:double.infinity, height: 250,),
+                  Text("รหัสสินค้า : ${widget.product['pcode']}",
+                    style: TextStyle(
+                        fontSize: 18
+                    ),
+                  ),
+
+                  Text("ชื่อไทย : ${widget.product['nproductMain']}",
+                      style: TextStyle(
+                          fontSize: 18
+                      ),
+                    ),
+
+                  Text("ชื่ออังกฤษ : ${widget.product['nproductENG']}",
+                    style: TextStyle(
+                        fontSize: 18
+                    ),
+                  ),
+                  DropdownButton(
+                    hint: Text("เลือกหน่วยสินค้า",style: TextStyle(fontSize: 18)),
+                    items: units.map((dropDownStringItem){
+                      return DropdownMenuItem<String>(
+                        value: dropDownStringItem,
+                        child: Text(dropDownStringItem, style: TextStyle(fontSize: 18)),
+                      );
+                    }).toList(),
+                    onChanged: (newValueSelected){
+                      _onDropDownItemSelected(newValueSelected);
+                      print(this._currentUnit);
+
+                    },
+                    value: _currentUnit,
+
+                  ),
+                  MaterialButton(
+                    color: Colors.deepOrange,
+                    textColor: Colors.white,
+                    minWidth: double.infinity,
+                    height: 50,
+                    child: Text(
+                      "หยิบใส่ตะกร้า",
+                      style: new TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold
+                      ),
+                    ),
+                    //onPressed: (){Navigator.pushReplacementNamed(context, '/Home');},
+                    onPressed: () {
+                    },
+                  )
+                ],
               ),
             ),
-          ),
-          Text(widget.product['nproductENG'].toString()),
-          MaterialButton(
-            color: Colors.deepOrange,
-            textColor: Colors.white,
-            minWidth: double.infinity,
-            height: 50,
-            child: Text(
-              "หยิบใส่ตะกร้า",
-              style: new TextStyle(
-                  fontSize: 20
-              ),
-            ),
-            //onPressed: (){Navigator.pushReplacementNamed(context, '/Home');},
-            onPressed: () {
-            },
-          )
-        ],
+          ],
+        ),
       ),
     );
 
+  }
+
+  _onDropDownItemSelected(newValueSelected){
+    setState(() {
+      this._currentUnit = newValueSelected;
+      print('select--${units}');
+    });
   }
 }
 
