7e1ae23debd43a472de82cd1cdd84a644d89d62b
wangAppPC <wanghatyai@gmail.com>
2019-03-19 17:48:08 +0700

    create page search

---
+++ lib/search.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:async';
+import 'dart:convert';
+
+class SearchPage extends StatefulWidget {
+  @override
+  _SearchPageState createState() => _SearchPageState();
+}
+
+class _SearchPageState extends State<SearchPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
