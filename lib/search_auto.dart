45f7a337a29d231847892d6e291176459ab3cc8c
wangAppPC <wanghatyai@gmail.com>
2019-04-01 18:07:22 +0700

    add fn search auto (not complete)

--- lib/search_auto.dart
+++ lib/search_auto.dart
@@ -12,6 +12,10 @@ class searchAutoPage extends StatefulWidget {
 
 class _searchAutoPageState extends State<searchAutoPage> {
 
+  AutoCompleteTextField searchTextField;
+  GlobalKey key;
+
+
   bool loading = true;
   var productAll = [];
 
@@ -53,20 +57,44 @@ class _searchAutoPageState extends State<searchAutoPage> {
     //print(json.decode(res.body));
   }
 
+  Widget row(product){
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: <Widget>[
+        Text(product.nproductMain),
+        SizedBox(width: 10),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomPadding: false,
       body: Column(
         mainAxisAlignment: MainAxisAlignment.start,
         children: <Widget>[
-          loading ? CircularProgressIndicator():
-          AutoCompleteTextField(
+          //loading ? CircularProgressIndicator():
+          searchTextField = AutoCompleteTextField(
+            key: key,
+            clearOnSubmit: false,
             suggestions: productAll,
             decoration: InputDecoration(
               hintText: "ค้นหา"
             ),
             itemFilter: (item, query){
-
+              return item.nproductMain.toLowerCase.startsWith(query.toLowerCase());
+            },
+            itemSorter: (a, b){
+              return a.nproductMain.compareTo(b.nproductMain);
+            },
+            itemSubmitted: (item){
+              setState(() {
+                searchTextField.textField.controller.text = item.nproductMain;
+              });
+            },
+            itemBuilder: (context, item){
+              return row(item);
             },
           )
         ],
