329a0b453a44f56719d2bcef9c3abf9787e9ad64
limbogPC <limbog.com@gmail.com>
2019-04-03 17:46:08 +0700

    change product model same youtube Tutorial https://www.youtube.com/watch?v=mYiyICUrtw8

--- lib/product_model.dart
+++ lib/product_model.dart
@@ -11,7 +11,7 @@ class Product{
   final String productUnit3;
 
 
-  Product(
+  Product({
     this.productId,
     this.productName,
     this.productCode,
@@ -20,29 +20,20 @@ class Product{
     this.productUnit1,
     this.productUnit2,
     this.productUnit3
-  );
+  });
 
-  Product.fromJson(Map<String, dynamic> json)
-    :
-        productId =  json['pID'],
-        productName = json['nproductMain'],
-        productCode = json['pcode'],
-        productNameENG = json['nproductENG'],
-        productPic = json['pic'],
-        productUnit1 = json['unit1'],
-        productUnit2 = json['unit2'],
-        productUnit3 = json['unit3'];
-
-  Map<String, dynamic> toJson() => {
-        'productId' : productId,
-        'productName' : productName,
-        'productCode' : productCode,
-        'productNameENG' : productNameENG,
-        'productPic' : productPic,
-        'productUnit1' : productUnit1,
-        'productUnit2' : productUnit2,
-        'productUnit3' : productUnit3,
-  };
+  factory Product.fromJson(Map<String, dynamic> json){
+    return new Product(
+    productId: json['pID'],
+    productName: json['nproductMain'],
+    productCode: json['pcode'],
+    productNameENG: json['nproductENG'],
+    productPic: json['pic'],
+    productUnit1: json['unit1'],
+    productUnit2: json['unit2'],
+    productUnit3: json['unit3'],
+    );
+  }
 
 
 }
