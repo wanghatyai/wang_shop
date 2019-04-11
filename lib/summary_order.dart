26e98693820e905d1eec39c878b34605e7b3384d
wangAppPC <wanghatyai@gmail.com>
2019-04-11 17:46:19 +0700

    create summaryOrder page

---
+++ lib/summary_order.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class SummaryOrderPage extends StatefulWidget {
+  @override
+  _SummaryOrderPageState createState() => _SummaryOrderPageState();
+}
+
+class _SummaryOrderPageState extends State<SummaryOrderPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
