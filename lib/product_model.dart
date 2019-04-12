8a85aa656c8c536449ffc6e42ce2e3d680b988b6
wangAppPC <wanghatyai@gmail.com>
2019-04-12 16:33:16 +0700

    add product price type

--- lib/product_model.dart
+++ lib/product_model.dart
@@ -9,6 +9,12 @@ class Product{
   final String productUnit1;
   final String productUnit2;
   final String productUnit3;
+  final String productP11;
+  final String productP12;
+  final String productP21;
+  final String productP22;
+  final String productP31;
+  final String productP32;
 
 
   Product({
@@ -19,19 +25,31 @@ class Product{
     this.productPic,
     this.productUnit1,
     this.productUnit2,
-    this.productUnit3
+    this.productUnit3,
+    this.productP11,
+    this.productP12,
+    this.productP21,
+    this.productP22,
+    this.productP31,
+    this.productP32
   });
 
   factory Product.fromJson(Map<String, dynamic> json){
     return new Product(
-    productId: json['pID'],
-    productName: json['nproductMain'],
-    productCode: json['pcode'],
-    productNameENG: json['nproductENG'],
-    productPic: json['pic'],
-    productUnit1: json['unit1'],
-    productUnit2: json['unit2'],
-    productUnit3: json['unit3'],
+      productId: json['pID'],
+      productName: json['nproductMain'],
+      productCode: json['pcode'],
+      productNameENG: json['nproductENG'],
+      productPic: json['pic'],
+      productUnit1: json['unit1'],
+      productUnit2: json['unit2'],
+      productUnit3: json['unit3'],
+      productP11: json['p11'],
+      productP12: json['p12'],
+      productP21: json['p21'],
+      productP22: json['p22'],
+      productP31: json['p31'],
+      productP32: json['p32'],
     );
   }
 
