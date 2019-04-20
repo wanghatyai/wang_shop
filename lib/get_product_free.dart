d8ae84254b6681fc780f96d1eebacbeace88f13e
wangAppPC <wanghatyai@gmail.com>
2019-04-20 17:27:09 +0700

    add get free product fn

---
+++ lib/get_product_free.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:wang_shop/database_helper.dart';
+import 'package:http/http.dart' as http;
+import 'dart:async';
+import 'dart:convert';
+
+class getProductFreePage extends StatefulWidget {
+  @override
+  _getProductFreePageState createState() => _getProductFreePageState();
+}
+
+class _getProductFreePageState extends State<getProductFreePage> {
+
+  List productFree = [];
+  var score;
+
+  getProduct() async{
+
+    final res = await http.get('http://wangpharma.com/API/product.php?Score=$score&act=Free');
+
+    if(res.statusCode == 200){
+
+      setState(() {
+        //isLoading = false;
+
+        var jsonData = json.decode(res.body);
+
+        jsonData.forEach((products) => productFree.add(products));
+        //perPage = productFree.length;
+
+        print(productFree);
+        print(productFree.length);
+
+        return productFree;
+
+      });
+
+
+    }else{
+      throw Exception('Failed load Json');
+    }
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getProduct();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Text('getFreeProduct');
+  }
+}
