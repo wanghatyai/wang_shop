4b153483e5e8d63210040e198ae38440a629cbd5
wangAppPC <wanghatyai@gmail.com>
2019-04-25 18:53:16 +0700

    add priceQty

--- lib/product_model.dart
+++ lib/product_model.dart
@@ -8,8 +8,11 @@ class Product{
   final String productNameENG;
   final String productPic;
   final String productUnit1;
+  final String productUnitQty1;
   final String productUnit2;
+  final String productUnitQty2;
   final String productUnit3;
+  final String productUnitQty3;
   final String productPriceA;
   final String productPriceB;
   final String productPriceC;
@@ -24,8 +27,11 @@ class Product{
     this.productNameENG,
     this.productPic,
     this.productUnit1,
+    this.productUnitQty1,
     this.productUnit2,
+    this.productUnitQty2,
     this.productUnit3,
+    this.productUnitQty3,
     this.productPriceA,
     this.productPriceB,
     this.productPriceC,
@@ -41,8 +47,11 @@ class Product{
       productNameENG: json['nproductENG'],
       productPic: json['pic'],
       productUnit1: json['unit1'],
+      productUnitQty1: json['unitQty1'],
       productUnit2: json['unit2'],
+      productUnitQty2: json['unitQty2'],
       productUnit3: json['unit3'],
+      productUnitQty3: json['unitQty3'],
       productPriceA: json['priceA'],
       productPriceB: json['priceB'],
       productPriceC: json['priceC'],
