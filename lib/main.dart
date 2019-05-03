7a0f22424ff44f490791951f6ae2441fe067d39f
limbogPC <limbog.com@gmail.com>
2019-05-04 06:22:55 +0700

    add bloc

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,10 @@ import 'package:wang_shop/database_helper.dart';
 
 import 'package:wang_shop/order.dart';
 
+import 'package:wang_shop/bloc_provider.dart';
+import 'package:wang_shop/bloc_count_order.dart';
+import 'package:wang_shop/bloc_count_order_all.dart';
+
 
 void main() {
 
@@ -28,27 +32,30 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'WangPharma',
-      routes: <String,WidgetBuilder>{
-        '/Home': (BuildContext context) => Home(),
-        '/Order': (BuildContext context) => OrderPage(),
-      },
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.green,
-      ),
-      home: LoginPage(),
-    );
+    return BlocProvider(
+        bloc: BlocCountOrder(),
+        child:  MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'WangPharma',
+          routes: <String,WidgetBuilder>{
+            '/Home': (BuildContext context) => Home(),
+            '/Order': (BuildContext context) => OrderPage(),
+          },
+          theme: ThemeData(
+            // This is the theme of your application.
+            //
+            // Try running your application with "flutter run". You'll see the
+            // application has a blue toolbar. Then, without quitting the app, try
+            // changing the primarySwatch below to Colors.green and then invoke
+            // "hot reload" (press "r" in the console where you ran "flutter run",
+            // or simply save your changes to "hot reload" in a Flutter IDE).
+            // Notice that the counter didn't reset back to zero; the application
+            // is not restarted.
+            primarySwatch: Colors.green,
+          ),
+          home: LoginPage(),
+        ),
+     );
   }
 }
 
