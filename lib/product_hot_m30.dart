5e374574a162c74af2854334d758ef665ac153e2
wangAppPC <wanghatyai@gmail.com>
2019-09-18 17:08:56 +0700

    update link detailProduct

--- lib/product_hot_m30.dart
+++ lib/product_hot_m30.dart
@@ -91,42 +91,49 @@ class _ProductHotMonth30PageState extends State<ProductHotMonth30Page> {
         //controller: _scrollController,
         gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
         itemBuilder: (context, int index){
-          return Card(
-              child: Column(
-                children: <Widget>[
-                  Expanded(
-                    child: Image.network('https://www.wangpharma.com/cms/product/${productTop[index].productPic}', fit: BoxFit.cover, width: 200,),
-                  ),
-                  Container(
-                    padding: EdgeInsets.fromLTRB(5, 0, 5, 0),
-                    //alignment: Alignment.topLeft,
-                    child: Column(
-                      children: <Widget>[
-                        //Text('${productTop[index].productCode}', style: TextStyle(fontWeight: FontWeight.bold)),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: <Widget>[
-                            Expanded(
-                              flex: 4,
-                              child: Text('${productTop[index].productName}', style: TextStyle(fontSize: 14), overflow: TextOverflow.ellipsis),
-                            ),
-                            Expanded(
-                              flex: 1,
-                              child: IconButton(
-                                  icon: Icon(Icons.add_to_photos, color: Colors.teal, size: 30,),
-                                  onPressed: (){
-                                    //addToOrderFast(productTop[index]);
-                                  }
-                              ),
-                            )
-                          ],
-                        )
-
-                      ],
+          return InkWell(
+            onTap: (){
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => productDetailPage(product: productTop[index])));
+            },
+            child: Card(
+                child: Column(
+                  children: <Widget>[
+                    Expanded(
+                      child: Image.network('https://www.wangpharma.com/cms/product/${productTop[index].productPic}', fit: BoxFit.cover, width: 200,),
                     ),
-                  ),
-                ],
-              )
+                    Container(
+                      padding: EdgeInsets.fromLTRB(5, 0, 5, 0),
+                      //alignment: Alignment.topLeft,
+                      child: Column(
+                        children: <Widget>[
+                          //Text('${productTop[index].productCode}', style: TextStyle(fontWeight: FontWeight.bold)),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: <Widget>[
+                              Expanded(
+                                flex: 4,
+                                child: Text('${productTop[index].productName}', style: TextStyle(fontSize: 14), overflow: TextOverflow.ellipsis),
+                              ),
+                              Expanded(
+                                flex: 1,
+                                child: IconButton(
+                                    icon: Icon(Icons.add_to_photos, color: Colors.teal, size: 30,),
+                                    onPressed: (){
+                                      //addToOrderFast(productTop[index]);
+                                    }
+                                ),
+                              )
+                            ],
+                          )
+
+                        ],
+                      ),
+                    ),
+                  ],
+                )
+            ),
           );
         },
         itemCount: productTop != null ? productTop.length : 0,
