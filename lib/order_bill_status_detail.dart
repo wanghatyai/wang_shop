ab811484e81f5e65a9278694a3ff81b9054c8c35
wangAppPC <wanghatyai@gmail.com>
2019-12-07 17:37:30 +0700

    add notification cart on appbar add func see detail product and add to cart add toast

--- lib/order_bill_status_detail.dart
+++ lib/order_bill_status_detail.dart
@@ -5,8 +5,11 @@ import 'dart:async';
 import 'dart:convert';
 import 'package:wang_shop/database_helper.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-
+import 'package:wang_shop/product_detail.dart';
+import 'package:wang_shop/product_model.dart';
 import 'package:wang_shop/order_bill_model.dart';
+
+import 'package:fluttertoast/fluttertoast.dart';
 
 import 'package:wang_shop/order.dart';
 
@@ -31,6 +34,7 @@ class _OrderBillStatusDetailPageState extends State<OrderBillStatusDetailPage> {
   DatabaseHelper databaseHelper = DatabaseHelper.internal();
 
   //Product product;
+  List <Product>productAll = [];
   List <OrderBill>orderBillDetailAll = [];
   bool isLoading = true;
   int perPage = 30;
@@ -54,7 +58,7 @@ class _OrderBillStatusDetailPageState extends State<OrderBillStatusDetailPage> {
     var orderBillProductSelectQty;
 
     final res = await http.get('https://wangpharma.com/API/orderBill.php?act=$act&orderID=${widget.orderID.orderBillMainId}');
-    //print('https://wangpharma.com/API/orderBill.php?act=$act&orderID=${widget.orderID.orderBillId}');
+    //print('https://wangpharma.com/API/orderBill.php?act=$act&orderID=${widget.orderID.orderBillMainId}');
 
     if(res.statusCode == 200){
 
@@ -64,8 +68,13 @@ class _OrderBillStatusDetailPageState extends State<OrderBillStatusDetailPage> {
         var jsonData = json.decode(res.body);
 
         jsonData.forEach((orderBills)=> orderBillDetailAll.add(OrderBill.fromJson(orderBills)));
+        jsonData.forEach((orderBills)=> productAll.add(Product.fromJson(orderBills)));
 
         for(var index = 0; index < orderBillDetailAll.length; index++){
+
+          //productAll[index].productId = orderBillDetailAll[index].orderBillProductID;
+          //productAll[index].productCode = orderBillDetailAll[index].orderBillProductCode;
+
 
           /*if(int.parse(orderBillDetailAll[index].orderBillProductProStatus) == 2){
             priceCredit = orderBillDetailAll[index].orderBillProductPriceA;
@@ -78,6 +87,8 @@ class _OrderBillStatusDetailPageState extends State<OrderBillStatusDetailPage> {
               priceCredit = orderBillDetailAll[index].orderBillProductPriceC;
             }
           }*/
+
+          print('product ID ${productAll[index].productId}');
 
           priceCredit = double.parse(orderBillDetailAll[index].orderBillProductSelectPrice);
           orderBillType = int.parse(orderBillDetailAll[index].orderBillProductSelectUnit);
@@ -131,6 +142,15 @@ class _OrderBillStatusDetailPageState extends State<OrderBillStatusDetailPage> {
     getOrderBillDetail();
   }
 
+  showToastAddFast(){
+    Fluttertoast.showToast(
+        msg: "เพิ่มรายการแล้ว",
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.CENTER,
+        timeInSecForIos: 3
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
 
@@ -138,8 +158,46 @@ class _OrderBillStatusDetailPageState extends State<OrderBillStatusDetailPage> {
 
     return Scaffold(
       appBar: AppBar(
-        //title: Text(widget.product.productName.toString()),
+        backgroundColor: Colors.green,
         title: Text("รายละเอียดรายการสินค้า"),
+        actions: <Widget>[
+          IconButton(
+              padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
+              icon: Stack(
+                children: <Widget>[
+                  Icon(Icons.shopping_cart, size: 40,),
+                  Positioned(
+                    right: 0,
+                    child: Container(
+                      padding: EdgeInsets.all(1),
+                      decoration: BoxDecoration(
+                        color: Colors.red,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      constraints: BoxConstraints(
+                        minWidth: 20,
+                        minHeight: 20,
+                      ),
+                      child: StreamBuilder(
+                        initialData: blocCountOrder.countOrder,
+                        stream: blocCountOrder.counterStream,
+                        builder: (BuildContext context, snapshot) => Text(
+                          '${snapshot.data}',
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 15
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              onPressed: (){
+                Navigator.push(context, MaterialPageRoute(builder: (context) => OrderPage()));
+              })
+        ],
       ),
       body: Column(
         children: <Widget>[
@@ -162,6 +220,9 @@ class _OrderBillStatusDetailPageState extends State<OrderBillStatusDetailPage> {
                     //setState(() {
                     //editOrderDialog(orders[index], 0);
                     //});
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => productDetailPage(product: productAll[index])));
                   },
                   leading: Image.network('https://www.wangpharma.com/cms/product/${orderBillDetailAll[index].orderBillProductPic}',fit: BoxFit.cover, width: 70, height: 70,),
                   title: Text('${orderBillDetailAll[index].orderBillProductName}', style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold), overflow: TextOverflow.ellipsis),
@@ -173,13 +234,13 @@ class _OrderBillStatusDetailPageState extends State<OrderBillStatusDetailPage> {
                         style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold, color: Colors.teal),),
                     ],
                   ),
-                  /*trailing: IconButton(
+                  trailing: IconButton(
                       icon: Icon(Icons.add_to_photos, color: Colors.teal, size: 40,),
                       onPressed: (){
-                        //addToOrderFast(productAll[index]);
+                        addToOrderFast(productAll[index]);
                       }
-                  ),*/
-                  trailing: Text('฿${formatter.format(priceNowAll[index]*int.parse(orderBillDetailAll[index].orderBillProductSelectQty))}', style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold, color: Colors.deepOrange)),
+                  ),
+                  //trailing: Text('฿${formatter.format(priceNowAll[index]*int.parse(orderBillDetailAll[index].orderBillProductSelectQty))}', style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold, color: Colors.deepOrange)),
                 );
               },
               itemCount: orderBillDetailAll != null ? orderBillDetailAll.length : 0,
@@ -188,5 +249,101 @@ class _OrderBillStatusDetailPageState extends State<OrderBillStatusDetailPage> {
         ],
       ),
     );
+  }
+
+  addToOrderFast(productFast) async{
+
+    var unit1;
+    var unit2;
+    var unit3;
+
+    int amount;
+
+    if(productFast.productUnit1.toString() != "null"){
+      unit1 = productFast.productUnit1.toString();
+    }else{
+      unit1 = 'NULL';
+    }
+    if(productFast.productUnit2.toString() != "null"){
+      unit2 = productFast.productUnit2.toString();
+    }else{
+      unit2 = 'NULL';
+    }
+    if(productFast.productUnit3.toString() != "null"){
+      unit3 = productFast.productUnit3.toString();
+    }else{
+      unit3 = 'NULL';
+    }
+
+    if(productFast.productProLimit != ""){
+
+      if(int.parse(productFast.productProLimit) > 0){
+        amount = int.parse(productFast.productProLimit);
+      }
+
+    }else{
+      amount = 1;
+    }
+
+    //print('99999-${productFast.productPriceA}');
+
+    Map order = {
+      'productID': productFast.productId.toString(),
+      'code': productFast.productCode.toString(),
+      'name': productFast.productName.toString(),
+      'pic': productFast.productPic.toString(),
+      'unit': productFast.productUnit1.toString(),
+      'unitStatus': 1,
+      'unit1': unit1,
+      'unitQty1': productFast.productUnitQty1,
+      'unit2': unit2,
+      'unitQty2': productFast.productUnitQty2,
+      'unit3': unit3,
+      'unitQty3': productFast.productUnitQty3,
+      'priceA': productFast.productPriceA,
+      'priceB': productFast.productPriceB,
+      'priceC': productFast.productPriceC,
+      'amount': amount,
+      'proStatus': productFast.productProStatus,
+    };
+
+    print(order);
+
+    var checkOrderUnit = await databaseHelper.getOrderCheck(order['code'], order['unit']);
+
+    //print(checkOrderUnit.isEmpty);
+
+    if(checkOrderUnit.isEmpty){
+
+      //print(order);
+      await databaseHelper.saveOrder(order);
+
+      showToastAddFast();
+
+      //add notify order
+      blocCountOrder.getOrderCount();
+
+    }else{
+
+      var sumAmount = checkOrderUnit[0]['amount'] + amount;
+      Map order = {
+        'id': checkOrderUnit[0]['id'],
+        'unit': checkOrderUnit[0]['unit'],
+        'unitStatus': 1,
+        'amount': sumAmount,
+      };
+
+      await databaseHelper.updateOrder(order);
+
+      showToastAddFast();
+
+
+      //add notify order
+      blocCountOrder.getOrderCount();
+
+    }
+
+    //Navigator.pushReplacementNamed(context, '/Home');
+
   }
 }
