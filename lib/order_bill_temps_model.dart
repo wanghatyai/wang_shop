d0b2e6508ec49ad79632959713965bb962cba2f0
limbogPC <limbog.com@gmail.com>
2020-04-17 14:48:41 +0700

    add orderBillDateAdd

--- lib/order_bill_temps_model.dart
+++ lib/order_bill_temps_model.dart
@@ -2,12 +2,14 @@ class OrderBillTemps{
   final String orderBillID;
   final String orderBillCode;
   final String orderBillCus;
+  final String orderBillDateAdd;
   final String orderBillSentStatus;
 
   OrderBillTemps({
     this.orderBillID,
     this.orderBillCode,
     this.orderBillCus,
+    this.orderBillDateAdd,
     this.orderBillSentStatus
   });
 
@@ -16,8 +18,8 @@ class OrderBillTemps{
       orderBillID: json['orderBill_ID'],
       orderBillCode: json['orderBill_Code'],
       orderBillCus: json['orderBill_Cus'],
+      orderBillDateAdd: json['orderBill_DateA'],
       orderBillSentStatus: json['sent_status'],
     );
   }
-
 }
