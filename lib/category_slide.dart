15c675787b4d08ed89a725a3922fa2a0c805cb19
phonggnohp <FuFuFu(GGG)073414632>
2021-07-21 10:31:30 +0700

    Update UI

--- lib/category_slide.dart
+++ lib/category_slide.dart
@@ -32,102 +32,105 @@ class _CategorySlidePageState extends State<CategorySlidePage> {
       color: Colors.white,
       //margin: EdgeInsets.symmetric(horizontal: marginValForDevice),
       height: heightCategorySlide,
-      child: ListView(
-        padding: EdgeInsets.fromLTRB(0, 10, 0, 0),
-        scrollDirection: Axis.horizontal,
-        children: <Widget>[
-          InkWell(
-            onTap: (){
-              Navigator.push(context, MaterialPageRoute(builder: (context) => ProductProPage()));
-              print('โปร');
-            },
-            child: Container(
-              padding: EdgeInsets.fromLTRB(15, 0, 15, 0),
-              child: Column(
-                children: <Widget>[
-                  Icon(Icons.assistant_photo, color: Colors.green, size: sizeCategorySlideIcon),
-                  Text('โปร', style: TextStyle(fontSize: 16))
-                ],
-              ),
-            )
-          ),
-          InkWell(
+      child: Center(
+        child: ListView(
+          shrinkWrap: true,
+          padding: EdgeInsets.fromLTRB(0, 10, 0, 0),
+          scrollDirection: Axis.horizontal,
+          children: <Widget>[
+            InkWell(
               onTap: (){
-                Navigator.push(context, MaterialPageRoute(builder: (context) => ProductHotPage()));
-                print('ขายดี');
+                Navigator.push(context, MaterialPageRoute(builder: (context) => ProductProPage()));
+                print('โปร');
               },
               child: Container(
                 padding: EdgeInsets.fromLTRB(15, 0, 15, 0),
                 child: Column(
                   children: <Widget>[
-                    Icon(Icons.monetization_on, color: Colors.amber, size: sizeCategorySlideIcon),
-                    Text('ขายดี', style: TextStyle(fontSize: 16))
+                    Icon(Icons.assistant_photo, color: Colors.green, size: sizeCategorySlideIcon),
+                    Text('โปร', style: TextStyle(fontSize: 16))
                   ],
                 ),
               )
-          ),
-          InkWell(
-              onTap: (){
-                Navigator.push(context, MaterialPageRoute(builder: (context) => ProductNewPage()));
-                print('ใหม่');
-              },
-              child: Container(
-                padding: EdgeInsets.fromLTRB(15, 0, 15, 0),
-                child: Column(
-                  children: <Widget>[
-                    Icon(Icons.fiber_new, color: Colors.red, size: sizeCategorySlideIcon),
-                    Text('ใหม่', style: TextStyle(fontSize: 16))
-                  ],
-                ),
-              )
-          ),
-          InkWell(
-              onTap: (){
-                Navigator.push(context, MaterialPageRoute(builder: (context) => ProductRecomPage()));
-                print('แนะนำ');
-              },
-              child: Container(
-                padding: EdgeInsets.fromLTRB(15, 0, 15, 0),
-                child: Column(
-                  children: <Widget>[
-                    Icon(Icons.thumb_up, color: Colors.blue, size: sizeCategorySlideIcon),
-                    Text('แนะนำ', style: TextStyle(fontSize: 16))
-                  ],
-                ),
-              )
-          ),
-          InkWell(
-              onTap: (){
-                Navigator.push(context, MaterialPageRoute(builder: (context) => ProductGimPage()));
-                print('ของแถม');
-              },
-              child: Container(
-                padding: EdgeInsets.fromLTRB(15, 0, 15, 0),
-                child: Column(
-                  children: <Widget>[
-                    Icon(Icons.tag_faces, color: Colors.deepPurple, size: sizeCategorySlideIcon),
-                    Text('ของแถม', style: TextStyle(fontSize: 16))
-                  ],
-                ),
-              )
-          ),
-          InkWell(
-              onTap: (){
-                Navigator.push(context, MaterialPageRoute(builder: (context) => ProductCategoryPage()));
-                print('หมวดหมู่');
-              },
-              child: Container(
-                padding: EdgeInsets.fromLTRB(15, 0, 15, 0),
-                child: Column(
-                  children: <Widget>[
-                    Icon(Icons.list, color: Colors.pink, size: sizeCategorySlideIcon),
-                    Text('หมวดหมู่', style: TextStyle(fontSize: 16))
-                  ],
-                ),
-              )
-          ),
+            ),
+            InkWell(
+                onTap: (){
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => ProductHotPage()));
+                  print('ขายดี');
+                },
+                child: Container(
+                  padding: EdgeInsets.fromLTRB(15, 0, 15, 0),
+                  child: Column(
+                    children: <Widget>[
+                      Icon(Icons.monetization_on, color: Colors.amber, size: sizeCategorySlideIcon),
+                      Text('ขายดี', style: TextStyle(fontSize: 16))
+                    ],
+                  ),
+                )
+            ),
+            InkWell(
+                onTap: (){
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => ProductNewPage()));
+                  print('ใหม่');
+                },
+                child: Container(
+                  padding: EdgeInsets.fromLTRB(15, 0, 15, 0),
+                  child: Column(
+                    children: <Widget>[
+                      Icon(Icons.fiber_new, color: Colors.red, size: sizeCategorySlideIcon),
+                      Text('ใหม่', style: TextStyle(fontSize: 16))
+                    ],
+                  ),
+                )
+            ),
+            InkWell(
+                onTap: (){
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => ProductRecomPage()));
+                  print('แนะนำ');
+                },
+                child: Container(
+                  padding: EdgeInsets.fromLTRB(15, 0, 15, 0),
+                  child: Column(
+                    children: <Widget>[
+                      Icon(Icons.thumb_up, color: Colors.blue, size: sizeCategorySlideIcon),
+                      Text('แนะนำ', style: TextStyle(fontSize: 16))
+                    ],
+                  ),
+                )
+            ),
+            InkWell(
+                onTap: (){
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => ProductGimPage()));
+                  print('ของแถม');
+                },
+                child: Container(
+                  padding: EdgeInsets.fromLTRB(15, 0, 15, 0),
+                  child: Column(
+                    children: <Widget>[
+                      Icon(Icons.tag_faces, color: Colors.deepPurple, size: sizeCategorySlideIcon),
+                      Text('ของแถม', style: TextStyle(fontSize: 16))
+                    ],
+                  ),
+                )
+            ),
+            InkWell(
+                onTap: (){
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => ProductCategoryPage()));
+                  print('หมวดหมู่');
+                },
+                child: Container(
+                  padding: EdgeInsets.fromLTRB(15, 0, 15, 0),
+                  child: Column(
+                    children: <Widget>[
+                      Icon(Icons.list, color: Colors.pink, size: sizeCategorySlideIcon),
+                      Text('หมวดหมู่', style: TextStyle(fontSize: 16))
+                    ],
+                  ),
+                )
+            ),
 
-        ],
+          ],
+        ),
       ),
     );
   }
